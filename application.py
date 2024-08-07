1ef6b229d717fac665320a2e57c3e206a1ae46eb
adityaranu37 <adityaranu359@gmail.com>
2024-08-07 14:08:07 +0530

    app.py to application.py updated

--- application.py
+++ application.py
@@ -47,7 +47,7 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")        
+    application.run(host="0.0.0.0")        
 
 
     # run app.py  and search at port http://127.0.0.1:5000/
